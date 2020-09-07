ed908371ce9a825d9275f71ae67a60418ff316c3
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2020-09-07 10:01:37 +0300

    using created ecs backend module

--- main.tf
+++ main.tf
@@ -18,10 +18,19 @@ module "frontend_prod" {
   root_domain_name = "prod.example.com"
 }
 
-
 module "frontend_staging" {
-  source   = "./publish_frontend/"
-  
-  route53_hosting_zone = "example.com"
-  root_domain_name = "staging.example.com"
+  source = "./publish_frontend/"
+
+  route53_hosting_zone = "bucket.network"
+  root_domain_name     = "staging.bucket.network"
+}
+
+
+module "backend_staging" {
+    source = "./publish_backend/"
+}
+
+
+output "staging_alb_hostname" {
+  value = module.backend_staging.alb_hostname
 }
