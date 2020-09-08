eb1da40a58b53f3cd44825fb6c9d360d2ab4d046
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2020-09-08 12:46:21 +0300

    remove alternative subjects to avoid changes during apply

--- publish_backend/create_certificate.tf
+++ publish_backend/create_certificate.tf
@@ -5,10 +5,6 @@ resource "aws_acm_certificate" "certificate" {
   // We want a wildcard cert so we can host subdomains later.
   domain_name       = var.root_domain_name
   validation_method = "DNS"
-
-  // We also want the cert to be valid for the root domain even though we'll be
-  // redirecting to the www. domain immediately.
-  subject_alternative_names = [var.root_domain_name]
 }
 
 data "aws_route53_zone" "example" {
